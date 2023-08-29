e54a3384f5d2481d97b5e63702d1754dd071c7fd
Ch Muhammad Tahir <tahir.ctn001@gmail.com>
2023-08-29 12:47:55 +0500

    Create Project structure and Add Fonts

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:e_commerce_store_ui/views/login_screen/login_screen_widget.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    theme: ThemeData(fontFamily: "Poppins"),
+    home: const LoginScreenWidget(),
+  ));
+}
---
+++ lib/views/home_page_screen/homepage_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreenWidget extends StatelessWidget {
+  const HomePageScreenWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Column(children: []),
+      ),
+    );
+  }
+}
