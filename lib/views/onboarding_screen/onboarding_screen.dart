bf15245a9a9c94e29d657ef49d610442321b63cf
Ch Muhammad Tahir <tahir.ctn001@gmail.com>
2023-08-30 23:29:10 +0500

    Add OnBoarding Screen

---
+++ lib/views/onboarding_screen/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreenWidget extends StatelessWidget {
+  const OnBoardingScreenWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: []),
+    );
+  }
+}
